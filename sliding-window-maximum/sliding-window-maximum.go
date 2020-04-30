2cbff96434b0e7eb28cf88aea75e29e1abee8924
yiwenlong <wlong.yi@gmail.com>
2020-04-30 11:17:26 +0800

    Algorithm for sliding window maximum & it's test case.

---
+++ sliding-window-maximum/sliding-window-maximum.go
@@ -0,0 +1,72 @@
+package sliding_window_maximum
+
+import "fmt"
+
+//给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+//返回滑动窗口中的最大值。
+//进阶：
+//你能在线性时间复杂度内解决此题吗？
+//
+//示例:
+//
+//输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+//输出: [3,3,5,5,6,7]
+//解释:
+//滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+//1 [3  -1  -3] 5  3  6  7       3
+//1  3 [-1  -3  5] 3  6  7       5
+//1  3  -1 [-3  5  3] 6  7       5
+//1  3  -1  -3 [5  3  6] 7       6
+//1  3  -1  -3  5 [3  6  7]      7
+// 
+//提示：
+//
+//1 <= nums.length <= 10^5
+//-10^4 <= nums[i] <= 10^4
+//1 <= k <= nums.length
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/sliding-window-maximum
+
+func enqueue(queue *[]int, k, num int)  {
+	if len(*queue) == 0 {
+		*queue = append(*queue, num)
+	}
+	if len(*queue) < k && (*queue)[0] > num {
+		*queue = append(*queue, num)
+		return
+	}
+	if len(*queue) < k && (*queue)[0] <= num {
+		*queue = []int{num}
+		return
+	}
+	if len(*queue) >= k {
+		*queue = (*queue)[len(*queue) - k + 1:]
+	}
+	*queue = append(*queue, num)
+	maxIndex := 0
+	for j := 0; j < len(*queue); j++ {
+		if (*queue)[j] >= (*queue)[maxIndex] {
+			maxIndex = j
+		}
+	}
+	*queue = (*queue)[maxIndex:]
+}
+// 思路：
+// 使用一个最大长度为 K 的队列维护窗口中的值。
+// 始终保持队首为窗口中的最大值。
+// 窗口滑动时，使队列最大值左侧的数全部出队。
+// 每滑动一次窗口，取队首的值添加到结果集合。
+func maxSlidingWindow(nums []int, k int) []int {
+	var queue, res []int
+	for i := 0; i < len(nums); i++ {
+		enqueue(&queue, k, nums[i])
+		fmt.Printf("queue: %v\n", queue)
+		if i >= (k - 1) {
+			res = append(res, queue[0])
+		}
+	}
+	return res
+}
---
+++ sliding-window-maximum/sliding-window-maximum_test.go
@@ -0,0 +1,41 @@
+package sliding_window_maximum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_maxSlidingWindow(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"normal",
+			args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 3},
+			[]int{3, 3, 5, 5, 6, 7},
+		},
+		{
+			"normal",
+			args{[]int{1, 3, 1, 2, 0, 5}, 3},
+			[]int{3, 3, 2, 5},
+		},
+		{
+			"normal",
+			args{[]int{9,10,9,-7,-4,-8,2,-6}, 5},
+			[]int{10,10,9,2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSlidingWindow(tt.args.nums, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("maxSlidingWindow(%v, %d) = %v, want %v", tt.args.nums, tt.args.k, got, tt.want)
+			}
+		})
+	}
+}
