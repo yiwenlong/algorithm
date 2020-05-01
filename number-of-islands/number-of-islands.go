3ed9e999ebdc907eec43d4351c75cb3d17bb45f9
yiwenlong <wlong.yi@gmail.com>
2020-05-01 23:52:59 +0800

    Modify algorithm for number of islands. Add a test case. Add algorithm description.

--- number-of-islands/number-of-islands.go
+++ number-of-islands/number-of-islands.go
@@ -1,6 +1,29 @@
 package main
 
 import "fmt"
+//
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+//此外，你可以假设该网格的四条边均被水包围。
+//
+//示例 1:
+//输入:
+//11110
+//11010
+//11000
+//00000
+//输出: 1
+//
+//示例 2:
+//输入:
+//11000
+//11000
+//00100
+//00011
+//输出: 3
+//解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+
+// https://leetcode-cn.com/problems/number-of-islands/
 
 func setZero(grid [][]byte, startI, startJ, bondI, bondJ int) {
 	grid[startI][startJ] = 0
@@ -13,7 +36,7 @@ func setZero(grid [][]byte, startI, startJ, bondI, bondJ int) {
 		setZero(grid, next, startJ, bondI, bondJ)
 	}
 	next = startJ - 1
-	if next >= 0 && grid[startI][startJ] == 1 {
+	if next >= 0 && grid[startI][next] == 1 {
 		setZero(grid, startI, next, bondI, bondJ)
 	}
 	next = startJ + 1
@@ -35,9 +58,6 @@ func numIslands(grid [][]byte) int {
 	return res
 }
 
-//[
-
-
 func main() {
 	grid := [][]byte {
 		{1,1,1,1,0},
@@ -55,6 +75,16 @@ func main() {
 		{1,1,0,1,0},
 		{0,0,1,0,0},
 		{0,0,0,1,1},
+	}
+	setZero(grid, 0, 0, len(grid), len(grid[0]))
+	for _, line := range grid{
+		fmt.Printf("%v\n", line)
+	}
+	fmt.Printf("\n")
+	grid = [][]byte {
+		{1,1,1},
+		{0,1,0},
+		{1,1,1},
 	}
 	setZero(grid, 0, 0, len(grid), len(grid[0]))
 	for _, line := range grid{
--- number-of-islands/number-of-islands_test.go
+++ number-of-islands/number-of-islands_test.go
@@ -29,6 +29,14 @@ func Test_numIslands(t *testing.T) {
 			}},
 			4,
 		},
+		{"testcase3",
+			args{[][]byte {
+				{1,1,1},
+				{0,1,0},
+				{1,1,1},
+			}},
+			1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
