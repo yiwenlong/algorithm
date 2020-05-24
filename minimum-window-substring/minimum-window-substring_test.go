a293c999b8f47a6e07a3b8b71fae1756276f1c7e
伊文龙 <wlong.yi@gmail.com>
2020-05-24 11:40:13 +0800

    TestCase for minimum-window-substring

---
+++ minimum-window-substring/minimum-window-substring_test.go
@@ -0,0 +1,25 @@
+package minimum_window_substring
+
+import "testing"
+
+func Test_minWindow(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{"a", "a"}, "a"},
+		{"1", args{"ADOBECODEBANC", "ABC"}, "BANC"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minWindow(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("minWindow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
